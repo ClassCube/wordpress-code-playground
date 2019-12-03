99c8203a26a4c5f25e0a1cc2f06223a4b092e826
Ryan Nutt <ryan@nutt.com>
2019-12-03 08:34:50 -0600

    Add GitHub updater class from SmashingMagazine

--- code-playground.php
+++ code-playground.php
@@ -17,3 +17,11 @@ $dir = plugin_dir_path( __FILE__ );
 require($dir . 'inc/Settings.php');
 require($dir . 'inc/User.php');
 require($dir . 'inc/Admin.php');
+
+if (!class_exists('\ClassCube\WordPress\Playground\Smashing_Updater')) {
+  require($dir . 'inc/Smashing_Updater.php'); 
+}
+$updater = new \ClassCube\WordPress\Playground\Smashing_Updater( __FILE__ );
+$updater->set_username( 'classcube' );
+$updater->set_repository( 'wordpress-code-playground' );
+$updater->initialize();
