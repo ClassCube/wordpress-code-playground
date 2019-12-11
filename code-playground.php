01ee18c9c0a8f98c0cc9fc8fa85018a00d0828b2
Ryan Nutt <ryan@nutt.com>
2019-12-11 12:44:58 -0600

    changed author;

--- code-playground.php
+++ code-playground.php
@@ -7,8 +7,8 @@
  * Version:           0.1.1
  * Requires at least: 5.2
  * Requires PHP:      7.2
- * Author:            Ryan Nutt
- * Author URI:        https://www.nutt.net
+ * Author:            ClassCube
+ * Author URI:        https://classcube.com
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       code-playground
